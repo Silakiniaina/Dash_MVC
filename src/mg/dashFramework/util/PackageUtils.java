1524bf4518102fcfa710b70c6e9b35e5961309ec
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-21 14:50:20 +0300

    listing all the controller

---
+++ src/mg/dashFramework/util/PackageUtils.java
@@ -0,0 +1,50 @@
+package mg.dashFramework.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PackageUtils {
+    public static List<Class<?>> getClasses(String packageName) throws ClassNotFoundException, IOException {
+        List<Class<?>> classes = new ArrayList<>();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+
+        URL resource = classLoader.getResource(path);
+
+        if (resource == null) {
+            return classes;
+        }
+
+        File packageDir = new File(resource.getFile().replace("%20", " "));
+        
+        for (File file : packageDir.listFiles()) {
+            if (file.isDirectory()) {
+                classes.addAll(PackageUtils.getClasses(packageName + "." + file.getName()));
+            } else {
+                String className = packageName + "." + FileUtils.getSimpleFileName(file.getName(), "class");
+                classes.add(Class.forName(className));
+            }
+        }
+        
+        return classes;
+    }
+
+    public static List<Class<?>> getClassesWithAnnotation(String packageName, Class<? extends Annotation> annotationClass) throws ClassNotFoundException, IOException {
+        List<Class<?>> classes = getClasses(packageName);
+
+        ArrayList<Class<?>> result = new ArrayList<Class<?>>();
+        
+        for(Class<?> clazz : classes) {
+            if (clazz.isAnnotationPresent(annotationClass)) {
+                result.add(clazz);
+            }
+        }
+
+        return result;
+    }
+
+}
