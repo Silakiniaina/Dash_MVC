bbaa7ea564e6d0546d97b35fd59bd6d08bff559a
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-28 10:01:16 +0300

    Creation annotation Get

---
+++ src/mg/dashFramework/annotation/Get.java
@@ -0,0 +1,7 @@
+package mg.dashFramework.annotation;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Get {}
