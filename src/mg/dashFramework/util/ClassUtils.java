58d538637f1766e600810c61e680cd48849c343a
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-28 10:27:11 +0300

    Creation fonction getListMethodsClass

--- src/mg/dashFramework/util/ClassUtils.java
+++ src/mg/dashFramework/util/ClassUtils.java
@@ -4,4 +4,13 @@ import java.lang.reflect.Method;
 
 public class ClassUtils{
 
+    /* Function to get all the methods in a class */
+    public static ArrayList<Method> getListMethodsClass(Class c){
+        ArrayList<Method> result = new ArrayList<Method>();
+        Method[] ls = c.getDeclaredMethods();
+        for(int i=0; i<ls.length; i++){
+            result.add(ls[i]);
+        }
+        return result;
+    }
 }
