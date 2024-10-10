36668e8dab4d620f1669796a80035445dd484d5f
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-10-10 10:18:19 +0300

    implementing a set of verbaction in mappin's attribute

--- src/mg/dashFramework/handler/url/Mapping.java
+++ src/mg/dashFramework/handler/url/Mapping.java
@@ -1,23 +1,31 @@
 package mg.dashFramework.handler.url;
 
-import java.lang.reflect.Method;
+import java.util.Set;
 
 public class Mapping{
     private String className;
+    private Set<VerbAction> listVerbActions;
 
     /* Constructor */
-    public Mapping(String className){
+    public Mapping(String className, Set<VerbAction> list){
         this.setClassName(className);
+        this.setListVerbActions(list);
     }
     
     /* Getters */
     public String getClassName(){
         return this.className;
     }
+    public Set<VerbAction> getListVerbActions(){
+        return this.listVerbActions;
+    }
 
     /* Setters */
     public void setClassName(String className){
         this.className = className;
+    }
+    public void setListVerbActions(Set<VerbAction> ls){
+        this.listVerbActions = ls;
     }
 
 }
