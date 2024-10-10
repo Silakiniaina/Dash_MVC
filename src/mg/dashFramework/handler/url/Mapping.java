4e6c36c6ceb8758cde3cf92ba0a70661ce2db0bb
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-10-10 10:15:19 +0300

    remove Method attribute in mapping

--- src/mg/dashFramework/handler/url/Mapping.java
+++ src/mg/dashFramework/handler/url/Mapping.java
@@ -4,28 +4,20 @@ import java.lang.reflect.Method;
 
 public class Mapping{
     private String className;
-    private Method meth;
 
     /* Constructor */
-    public Mapping(String className, Method m){
+    public Mapping(String className){
         this.setClassName(className);
-        this.setMethod(m);
     }
     
     /* Getters */
     public String getClassName(){
         return this.className;
     }
-    public Method getMethod(){
-        return this.meth;
-    }
 
     /* Setters */
     public void setClassName(String className){
         this.className = className;
-    }
-    public void setMethod(Method m){
-        this.meth = m;
     }
 
 }
