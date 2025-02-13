e66bd7c480b895614edbf7b96261bb0991cc49fd
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-13 08:50:30 +0300

    create function getFileName(Part part)

--- src/mg/dash/mvc/util/FileUtils.java
+++ src/mg/dash/mvc/util/FileUtils.java
@@ -1,7 +1,21 @@
 package mg.dash.mvc.util;
 
+import jakarta.servlet.http.Part;
+
 public class FileUtils {
     public static String getSimpleFileName(String fileName, String extension) {
         return fileName.substring(0, (fileName.length() - extension.length()) - 1);
+    }
+
+    public static String getFileName(Part part) {
+        if (part == null) {
+            return null;
+        }
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(content.indexOf("=") + 2, content.length() - 1);
+            }
+        }
+        return null;
     }
 }
