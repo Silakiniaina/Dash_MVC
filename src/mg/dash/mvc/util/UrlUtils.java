39f4aba93d5308f06745af37aa51bff6be0e05ec
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-15 16:04:12 +0300

    feat : create function getOriginalUrl in UrlUtils

--- src/mg/dash/mvc/util/UrlUtils.java
+++ src/mg/dash/mvc/util/UrlUtils.java
@@ -1,5 +1,21 @@
 package mg.dash.mvc.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class UrlUtils {
     
+    public static String getOriginalUrl(HttpServletRequest request) {
+        String originalUrl = (String) request.getAttribute("javax.servlet.forward.request_uri");
+        if (originalUrl == null) {
+            originalUrl = request.getHeader("Referer");
+            if (originalUrl != null && originalUrl.startsWith("http://") || originalUrl.startsWith("https://")) {
+                originalUrl = originalUrl.substring(originalUrl.indexOf(request.getContextPath()));
+            }
+        }
+        if (originalUrl == null) {
+            originalUrl = request.getRequestURI();
+        }
+
+        return originalUrl;
+    }
 }
