07002f87c87f999a318322f16212559dc50f18e9
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-15 13:54:07 +0300

    feat : create function isDate in StringUtils

--- src/mg/dash/mvc/util/StringUtils.java
+++ src/mg/dash/mvc/util/StringUtils.java
@@ -1,5 +1,9 @@
 package mg.dash.mvc.util;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class StringUtils {
     
     public static boolean isNumeric(String str) {
@@ -7,6 +11,20 @@ public class StringUtils {
             Double.parseDouble(str);
             return true;
         } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isDate(String str) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            java.util.Date utilDate = dateFormat.parse(str);
+
+            Date sqlDate = new Date(utilDate.getTime());
+
+            return sqlDate != null;
+        } catch (ParseException e) {
             return false;
         }
     }
