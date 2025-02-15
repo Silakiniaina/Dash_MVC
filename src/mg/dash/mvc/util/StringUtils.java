7728c03c84670ff497a68919fb6b3e2ddf51aaa5
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-15 13:53:26 +0300

    feat : create function isNumeric in StringUtils

--- src/mg/dash/mvc/util/StringUtils.java
+++ src/mg/dash/mvc/util/StringUtils.java
@@ -2,4 +2,12 @@ package mg.dash.mvc.util;
 
 public class StringUtils {
     
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
