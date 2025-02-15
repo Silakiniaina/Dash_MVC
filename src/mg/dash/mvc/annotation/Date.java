8e53c4746be151003c55d58add7b90f4b31a45aa
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-15 13:46:16 +0300

    feat : create annotation Date

---
+++ src/mg/dash/mvc/annotation/Date.java
@@ -0,0 +1,10 @@
+package mg.dash.mvc.annotation;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Date {
+    
+    String format() default "yyyy-MM-dd";
+}
