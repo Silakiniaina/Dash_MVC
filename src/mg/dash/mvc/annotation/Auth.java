0b2e995233d9604b5c69447697b1d19e56f7c704
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-16 14:23:22 +0300

    feat : add required and roles in Auth

--- src/mg/dash/mvc/annotation/Auth.java
+++ src/mg/dash/mvc/annotation/Auth.java
@@ -4,7 +4,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Retention;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Auth {
-    
-    String value();
+
+    String[] roles() default {};  
+    boolean required() default true; 
 }
