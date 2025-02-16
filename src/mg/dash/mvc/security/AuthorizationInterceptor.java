36bf532858732ce2b84020c779ade3cd1a28bf53
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-16 14:55:46 +0300

    refactor : remove unused import

--- src/mg/dash/mvc/security/AuthorizationInterceptor.java
+++ src/mg/dash/mvc/security/AuthorizationInterceptor.java
@@ -1,13 +1,10 @@
 package mg.dash.mvc.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import mg.dash.mvc.annotation.Auth;
 import mg.dash.mvc.controller.MySession;
 import mg.dash.mvc.handler.exeption.AuthorizationException;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Set;
 
 public class AuthorizationInterceptor {
 public static void checkAuthorization(Method method, MySession mySession) throws AuthorizationException {
