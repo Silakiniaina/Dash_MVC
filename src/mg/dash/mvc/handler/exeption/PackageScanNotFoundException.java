ec30175f65340f9a12b4f7d3a577d8ef637f149a
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-16 13:35:00 +0300

    feat : add constructor for PackageScanNotFoundException

--- src/mg/dash/mvc/handler/exeption/PackageScanNotFoundException.java
+++ src/mg/dash/mvc/handler/exeption/PackageScanNotFoundException.java
@@ -2,4 +2,7 @@ package mg.dash.mvc.handler.exeption;
 
 public class PackageScanNotFoundException extends Exception{
     
+    public PackageScanNotFoundException(String message){
+        super(message);
+    }
 }
