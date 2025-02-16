ab808e86bf43f96198bbc27870bce1cff514016f
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-16 14:27:51 +0300

    feat : create class AuthorizationInterceptor

---
+++ src/mg/dash/mvc/security/AuthorizationInterceptor.java
@@ -0,0 +1,13 @@
+package mg.dash.mvc.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import mg.dash.mvc.annotation.Auth;
+import mg.dash.mvc.handler.exeption.AuthorizationException;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
+
+public class AuthorizationInterceptor {
+    
+}
